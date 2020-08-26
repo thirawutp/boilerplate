8c1dadc1352384a0356cb0a72f3d646da67e74be
thirawutp <thirawut.p@appman.co.th>
2020-08-26 15:54:38 +0700

    add l

--- packages/nativeapp/App.js
+++ packages/nativeapp/App.js
@@ -35,7 +35,7 @@ const App: () => React$Node = () => {
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Channel</Text>
+              <Text style={styles.footer}>Engine: Channell</Text>
             </View>
           )}
           <View style={styles.body}>
